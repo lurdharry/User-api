a1391e15ff9e368b5c6ac49d26fc70750b13b49c
lurdharry <azeezhammed57@gmail.com>
2023-09-07 17:15:59 +0100

    chore: add controller routes

--- main.go
+++ main.go
@@ -7,27 +7,23 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+
+
 func main()  {
 	e := echo.New()
 
 
-	md := &configs.MongoDriver{}
+
 
 	// Create Mongo connection
-	md.Connect()
+	configs.Connect()
 	// Close connection after main
-   defer md.Disconnect()
+//    defer md.Disconnect()
 	
 
    //routes
    routes.UserRole(e) 
 
-
-
-
-	e.GET("/", func(c echo.Context) error {
-		return  c.JSON(200, echo.Map{"data": "Hello from Echo & mongoDB"})
-	})
 
 	e.Logger.Fatal(e.Start(":6000"))
 }
--- routes/user_route.go
+++ routes/user_route.go
@@ -1,7 +1,16 @@
 package routes
 
-import "github.com/labstack/echo/v4"
+import (
+	"user-api/controllers"
+
+	"github.com/labstack/echo/v4"
+)
 
 func UserRole( e *echo.Echo)  {
 	
+	e.POST("/user", controllers.CreateUser )
+	e.GET("user/:userId",controllers.GetUser)
+	e.PUT("user/:userId",controllers.EditUser)
+	e.DELETE("user/:userId",controllers.DeleteUser)
+	e.GET("/users", controllers.GetAllUsers) 
 }
